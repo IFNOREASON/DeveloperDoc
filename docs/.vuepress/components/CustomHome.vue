14d6ba916a001b80063445e702b4481c3719fa81
meidan <meidandan570@163.com>
2024-02-22 16:12:29 +0800

    add

---
+++ docs/.vuepress/components/CustomHome.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="custom-home">
+    <header class="hero">
+      <!-- 添加你的标题和介绍 -->
+      <h1>我的 VuePress 站点</h1>
+      <p>这里是我的站点介绍。</p>
+    </header>
+    <!-- 其他自定义内容 -->
+    <!-- ... -->
+  </div>
+</template> 
+<script>
+// 可以在这里添加一些脚本逻辑
+//  export default { // 这里是 Vue 组件的选项 }
+</script> 
+<style>
+/* 在这里添加样式 */
+</style>
